e3dfe1f4f7272729ef261149a150e199360a1875
paloga87 <pame.loga@gmail.com>
2018-11-09 08:54:47 -0500

    JS

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 
-
 var letters;
 
+//Funcion Anagramas
 function capture(){
-  letters = document.getElementById("letters").value.split("").sort();
-  
-  for (var i = 0; i < dictionary.length;  i++){
-    //var palabra = dictionary[i].split("").sort();
-    
+  letters = document.getElementById("letters").value.split("").sort();  
+  // Recorre las palabras del diccionario
+  for (var i = 0; i < dictionary.length;  i++){   
+    // Recorre las letras de las palabras del diccionario
     for (var j = 0; j < dictionary[i].length;  j++){
-      var noAccent = quitaAcentos(dictionary[i][j]);
+      // Quita las tildes
+      var noAccent = removeAccent(dictionary[i][j]);
+      // Verifica que las letras coincidan
       var idx = letters.indexOf(noAccent);
+      // Filtra las que no coinciden
       if (idx !== -1) {
+        // Filtra las plabras que cumplen con todas las letras
         if (j == dictionary[i].length - 1) {
           var words = document.getElementById('words');
+          // las incluye en el HTML
           var newWord = document.createElement('li');
           newWord.textContent = dictionary[i];
           words.appendChild(newWord);
         }
-
       }else{
         j = dictionary[i].length + 1;
       }   
@@ -27,12 +30,9 @@ function capture(){
   } 
 }
 
-
-
-
-function quitaAcentos(str){ 
-for (var i=0;i<str.length;i++){ 
 //Sustituye "á é í ó ú" 
+function removeAccent(str){ 
+for (var i=0;i<str.length;i++){ 
 if (str.charAt(i)=="á") str = str.replace(/á/,"a"); 
 if (str.charAt(i)=="é") str = str.replace(/é/,"e"); 
 if (str.charAt(i)=="í") str = str.replace(/í/,"i"); 
